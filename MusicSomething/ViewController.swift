289aeff3e564cf6a37b12a6680e63708d725acae
Sam Meech-Ward <sam@meech-ward.com>
2017-08-24 09:16:17 -0700

    Initial Commit

---
+++ MusicSomething/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MusicSomething
+//
+//  Created by Sam Meech-Ward on 2017-08-24.
+//  Copyright © 2017 Sam Meech-Ward. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
